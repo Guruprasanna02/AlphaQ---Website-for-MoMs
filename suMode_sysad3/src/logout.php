3bfebe1c48edda2e423a89459a66bae34cfb58f2
Guruprasanna V <guruprasannavasudevan02@gmail.com>
2021-07-28 01:23:35 +0530

    Delete suMode_sysad3 directory

--- suMode_sysad3/src/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset();
-session_destroy();
-
-setcookie('unamecookie','',time()-86400);
-setcookie('passcookie','',time()-86400);
-header('location:login.php');
--- suMode_sysad3/src/partials/_dbconnect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$servername = "db";
-$username = "root";
-$password = "password";
-$database = "AlphaQMoM";
-
-$conn = mysqli_connect($servername, $username, $password, $database);
