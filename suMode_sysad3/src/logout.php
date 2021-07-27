f0580d46e9121e3fefd76cfd9463c047d9e2cc8f
Guruprasanna V <guruprasannavasudevan02@gmail.com>
2021-07-27 15:36:01 +0530

    Add files via upload

---
+++ suMode_sysad3/src/logout.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+session_unset();
+session_destroy();
+
+setcookie('unamecookie','',time()-86400);
+setcookie('passcookie','',time()-86400);
+header('location:login.php');
---
+++ suMode_sysad3/src/partials/_dbconnect.php
@@ -0,0 +1,7 @@
+<?php
+$servername = "db";
+$username = "root";
+$password = "password";
+$database = "AlphaQMoM";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
